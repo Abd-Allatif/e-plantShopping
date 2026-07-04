9e2b6642809c57b2ab8e6904af242b9fb3e4221f
Abd-Allatif <abd8allatif8@gmail.com>
2026-07-04 10:56:08 +0300

    updated the CartSlice with the code needed to add, remove and update product to the cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const {name, img, cost} = action.payload;
+
+      const existingItems = state.find((item)=> {item.name === name});
+
+      if(existingItems){
+        existingItems.quantity++;
+      }else{
+        existingItems.push({name, img, cost, quantity: 1});
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name != action.payload);
     },
     updateQuantity: (state, action) => {
+      const {name, quantity} = action.payload;
 
+      const itemsToUpdate = state.items.find((item) => item.name === name);
+
+      if(itemsToUpdate){
+        itemsToUpdate.quantity = quantity;
+      }
     
     },
   },
